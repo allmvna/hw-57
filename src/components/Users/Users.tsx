d2ba72a92fc4ac71ecd5f8bdc926ab5e5d9bbdab
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-10-03 14:55:41 +0600

    Add component 'UserItem' for displaying cards with information about users.

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,13 @@
+import UserItem from "./UserItem.tsx";
+
+const Users = () => {
+    return (
+        <div>
+            <UserItem/>
+            <UserItem/>
+            <UserItem/>
+        </div>
+    );
+};
+
+export default Users;
